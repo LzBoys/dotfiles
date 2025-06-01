1c21c5850dafbccf12397f34ea1c44e1be3ec9b1
Lincoln Zhu <lincolnzhu21@gmail.com>
2025-06-01 00:23:30 -0500

    added fish functions for g++

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -3,5 +3,15 @@ if status is-interactive
     eval (ssh-agent -c)
     set -gx EDITOR vim
     loadhyprland
+
+    function co
+	g++ -std=c++17 -O2 -o (path change-extension '' $argv[1]) $argv[1] -Wall
+    end
+
+    function run
+	set binary (path change-extension '' $argv[1])
+	co $argv[1] && ./{$binary} & fg
+    end
+
     starship init fish | source
 end
